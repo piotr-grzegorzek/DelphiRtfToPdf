e0681533912cbbfa8709b2b798f9187d2fe54edd
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-09-14 14:48:45 +0200

    feat: cli

---
+++ CLI/CLI/Program.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using Syncfusion.OfficeChart;
+using Syncfusion.Pdf;
+using System;
+using System.IO;
+
+namespace CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                WriteError("InvalidArgLength", "Usage: cli.exe <input_path> <output_pdf_path>");
+                return;
+            }
+
+            string inputPath = args[0];
+            string outputPdfPath = args[1];
+
+            if (!File.Exists(inputPath))
+            {
+                WriteError("FileNotFound", $"The input file '{inputPath}' does not exist.");
+                return;
+            }
+
+            try
+            {
+                ConvertToPdf(inputPath, outputPdfPath);
+                WriteSuccess($"Conversion successful. PDF saved to '{outputPdfPath}'.");
+            }
+            catch (Exception ex)
+            {
+                WriteError("ConversionError", ex.Message);
+            }
+        }
+
+        static void ConvertToPdf(string inputPath, string outputPdfPath)
+        {
+            using (FileStream fileStream = new(Path.GetFullPath(inputPath), FileMode.Open))
+            {
+                //Loads an existing Word document.
+                using (WordDocument wordDocument = new(fileStream, Syncfusion.DocIO.FormatType.Automatic))
+                {
+                    //Creates an instance of DocIORenderer.
+                    using (DocIORenderer renderer = new())
+                    {
+                        //Sets Chart rendering Options.
+                        renderer.Settings.ChartRenderingOptions.ImageFormat = ExportImageFormat.Jpeg;
+                        //Converts Word document into PDF document.
+                        using (PdfDocument pdfDocument = renderer.ConvertToPDF(wordDocument))
+                        {
+                            //Saves the PDF file to file system.    
+                            using (FileStream outputStream = new(Path.GetFullPath(outputPdfPath), FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
+                            {
+                                pdfDocument.Save(outputStream);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        static void WriteError(string code, string message)
+        {
+            var error = new
+            {
+                status = "error",
+                code,
+                message
+            };
+
+            Console.WriteLine(JsonConvert.SerializeObject(error));
+        }
+
+        static void WriteSuccess(string message)
+        {
+            var success = new
+            {
+                status = "success",
+                message
+            };
+
+            Console.WriteLine(JsonConvert.SerializeObject(success));
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,36 @@
+# Delphi RTF to PDF
+
+## Overview
+
+CLI/* contains source code for .NET 5 (default target to make it [work on win7](https://github.com/dotnet/core/blob/main/release-notes/5.0/5.0-supported-os.md), there is also .net 8 [sync fusion example](https://github.com/SyncfusionExamples/DocIO-Examples/blob/main/Word-to-PDF-Conversion/Convert-Word-document-to-PDF/.NET/Convert-Word-document-to-PDF/Convert-Word-document-to-PDF.csproj)) CLI.
+
+Delphi/* contains source code for delphi wrapper parsing the CLI output, along with error handling, so you can easily integrate it into your delphi application, by checking error types below.
+
+## Errors
+
+- **InvalidArgLength** - Error while parsing arguments
+
+- **FileNotFoundErr** - Input file not found
+
+- **UnexpectedErr** - Error while converting file
+
+## Supported formats
+
+Supported input formats are available [here](https://help.syncfusion.com/document-processing/word/conversions/word-to-pdf/net/word-to-pdf?cs-save-lang=1&cs-lang=csharp#supported-file-formats).
+
+## Example
+
+All you need is compiled .NET CLI and Delphi wrapper in your uses clause.
+
+```delphi
+```
+
+## SyncFusion license
+
+SyncFusion is prioprietary software, so you need to have a license to use it ([there is also a community version](https://www.syncfusion.com/products/communitylicense))
+
+## Nuget Dependencies
+
+- [Syncfusion.DocIORenderer.Net.Core](https://www.nuget.org/packages/Syncfusion.DocIORenderer.Net.Core)
+
+- [Newtonsoft.Json](https://www.nuget.org/packages/newtonsoft.json/)
